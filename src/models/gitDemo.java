df2903500663f2d062e2d3d07050cf27a731b707
Kovvuri <srinivasa-reddy.kovvuri@capgemini.com>
2024-09-03 15:51:18 +0530

    changes made in project

--- src/models/gitDemo.java
+++ src/models/gitDemo.java
@@ -4,5 +4,6 @@ public class gitDemo {
 	public static void main(String args[]) {
 		System.out.println("this is demo git");
 		System.out.println("Output of pull");
+		System.out.println("Changes made in project");
 	}
 }
